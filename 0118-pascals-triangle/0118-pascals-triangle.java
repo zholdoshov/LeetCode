18509a2777f18db628009e18a1de4772fd4e1d02
Nurulla Zholdoshov <nur.joldoshov@gmail.com>
2022-11-23 02:05:51 -0800

    Time: 0 ms (100.00%), Space: 40 MB (92.80%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
+        
         for (int i = 1; i <= numRows; i++) {
             List<Integer> row = new ArrayList<>();
+            
             for (int j = 0; j < i; j++){
+                
                 if (j == 0 || j == i-1) {
-                    row.add(1);
+                    row.add(1); 
                 } else {
                     row.add(triangle.get(i-2).get(j-1) + triangle.get(i-2).get(j));
                 }
             }
             triangle.add(row);
         }
+        
         return triangle;
     }
 }
+
+/* if row's index is 0 or row.length-1 then apply 1 for that indexes else
+sum of (j-1's element of the row and j's element of the row)
+*/
